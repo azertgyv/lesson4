a93b941f7338d6694e791c63072ba75f1859383b
azertgyv <atagiyev077@gmail.com>
2023-05-06 21:31:56 +0400

    lesson4

---
+++ app.js
@@ -0,0 +1,55 @@
+
+// function cem(num1,num2){
+//     console.log(num1 + num2)
+// }
+// cem(12,23) 
+
+
+// function pow(num1,num2){
+//                  console.log(num1 ** num2)
+// }
+// pow(3,4  )
+
+
+
+// TASK 1
+
+// const num = +prompt("Bir eded daxil edin: "); 
+
+// const reversedNum = () => num.toString().split("").reverse().join(""); 
+
+// console.log((reversedNum(num)));
+
+
+// TASK 2
+
+// let x = Math.floor(Math.random() * 100 + 1);
+// console.log(x)
+
+
+// TASK 3
+
+// let num1 = +prompt('Eded daxil edin: ')
+// let num2 = +prompt('Eded daxil edin: ')
+
+//  numQuvvet = num1 ** num2
+//  console.log(numQuvvet)
+
+// TASK 4
+
+// let num1 = +prompt('1.ci ededi daxil edin: ')
+// let num2 = +prompt('2.ci ededi daxil edin: ')
+
+// const sum = Number(num1 + num2)
+// const multiply = Number(num1 - num2)
+// const divide = Number(num1 ** num2)
+// const sqrt = Number(num1 /num2)
+// const subtract = Number(num1 * num2)
+
+// console.log(sum)
+// console.log(multiply)
+// console.log(divide)
+// console.log(sqrt)
+// console.log(subtract)
+
+
